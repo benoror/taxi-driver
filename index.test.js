f2cb75500e487c5f58039791a37948c8de468647
Ben Orozco <benoror@gmail.com>
2018-11-09 15:36:00 -0600

    Test[MEX]: RET_IVA

--- index.test.js
+++ index.test.js
@@ -10,5 +10,14 @@ describe('MEXICO', () => {
 
     expect(getTax(o)).toBe(0.16);
   });
+
+  test('RET_IVA', () => {
+    const o = {
+      country: 'MX',
+      taxType: 'RET_IVA'
+    };
+
+    expect(getTax(o)).toBe(0.167);
+  });
 });
 

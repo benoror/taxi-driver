31b665605ce10087981d61fbc350945eee9b81f5
Ben Orozco <benoror@gmail.com>
2018-11-09 20:09:28 -0600

    Test: All tests for MEXICO

--- index.test.js
+++ index.test.js
@@ -1,29 +1,58 @@
 const getTax = require('./index');
 
-
 describe('MEXICO', () => {
-  test('IVA', () => {
-    const o = {
-      country: 'MX',
-      taxType: 'IVA'
-    };
+  describe('IVA', () => {
+    test('IVA NORMAL', () => {
+      const o = {
+        country: 'MEX',
+        taxType: 'IVA'
+      };
 
-    expect(getTax(o)).toEqual(
-      expect.objectContaining({
-        value: 0.16
-      })
-    );
+      expect(getTax(o)).toEqual(
+        expect.objectContaining({
+          value: 0.16
+        })
+      );
+    });
+
+    test('IVA DRUG PHARMACY', () => {
+      const o = {
+        country: 'MEX',
+        taxType: 'IVA',
+        category: 'DRUG',
+        area: 'PHARMACY'
+      };
+
+      expect(getTax(o)).toEqual(
+        expect.objectContaining({
+          value: 0
+        })
+      );
+    });
   });
 
   test('RET_IVA', () => {
     const o = {
-      country: 'MX',
+      country: 'MEX',
       taxType: 'RET_IVA'
     };
 
     expect(getTax(o)).toEqual(
       expect.objectContaining({
         value: 0.167
+      })
+    );
+  });
+
+  test('RET_ISR', () => {
+    const o = {
+      country: 'MEX',
+      taxType: 'RET_ISR'
+    };
+
+    expect(getTax(o)).toEqual(
+      expect.objectContaining({
+        value: 0.1
       })
     );
   });

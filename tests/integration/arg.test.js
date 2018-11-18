02acb7fea626c6e660e6fbcec30db011551a6dbc
Ben Orozco <benoror@gmail.com>
2018-11-17 19:41:32 -0600

    Test[rules]: More ARG tests

--- tests/integration/arg.test.js
+++ tests/integration/arg.test.js
@@ -2,7 +2,7 @@ const { getTax } = require('../../getTax');
 
 describe('Argentina', () => {
   describe('Inscripto', () => {
-    test('>10700', () => {
+    test('>10700 <=15000', () => {
       const q = {
         txType: "sales",
         docType: "payment",
@@ -15,7 +15,43 @@ describe('Argentina', () => {
 
       expect(getTax('ar', q)).toEqual(
         expect.objectContaining({
-          result: 1284.12
+          result: 10701*0.12 // 1284.12
+        })
+      );
+    });
+
+    test('>15000 <=20000', () => {
+      const q = {
+        txType: "sales",
+        docType: "payment",
+        category: 'ALQUILERES_RURAL',
+        bpTaxType: 'INSCRIPTO',
+        vars: {
+          productTotal: '19999'
+        }
+      };
+
+      expect(getTax('ar', q)).toEqual(
+        expect.objectContaining({
+          result: 19999*0.15 // 2,999.85
+        })
+      );
+    });
+
+    test('>40000 <=60000', () => {
+      const q = {
+        txType: "sales",
+        docType: "payment",
+        category: 'ALQUILERES_RURAL',
+        bpTaxType: 'INSCRIPTO',
+        vars: {
+          productTotal: '60000'
+        }
+      };
+
+      expect(getTax('ar', q)).toEqual(
+        expect.objectContaining({
+          result: 60000*0.27 // 16,200
         })
       );
     });
@@ -33,7 +69,7 @@ describe('Argentina', () => {
 
       expect(getTax('ar', q)).toEqual(
         expect.objectContaining({
-          result: 642
+          result: 10700*0.06 // 642
         })
       );
     });
@@ -53,7 +89,7 @@ describe('Argentina', () => {
 
       expect(getTax('ar', q)).toEqual(
         expect.objectContaining({
-          result: 2996.28
+          result: 10701*0.28 // 2996.28
         })
       );
     });

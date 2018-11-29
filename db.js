2eb6a6ce4767f5e6b247e1285d26535ee8596788
Ben Orozco <benoror@gmail.com>
2018-11-28 21:02:13 -0600

    Ref[now]: Export DB data as a POJO

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const low = require('lowdb');
 const Memory = require('lowdb/adapters/Memory')
-const data = require('./db.json');
+const data = require('./db_data');
 const db = low(new Memory());
 
 db.defaults(data)
